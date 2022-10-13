9369913a0de1f8c4fe0f5dadc1616a069fe62962
martin <mawisor@gmail.com>
2022-10-12 22:12:31 -0400

    first commit

---
+++ refresher.js
@@ -0,0 +1,17 @@
+console.log("Hi");
+
+var firstName = "Martin";
+var lastName = "Wisor";
+var fullName = `${firstName + lastName}`;
+console.log(fullName);
+let population = 670000;
+console.log(population);
+console.log(population * 3);
+let javaScriptIsCool = true;
+let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
---
+++ workingWithStrings.js
@@ -0,0 +1,7 @@
+let name = "Wilson";
+
+// final message should say: Welcome back <name>!
+
+// console.log("Welcome back " + name + "!");
+
+console.log(`Welcome back ${name}!`);
